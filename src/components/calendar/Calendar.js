adf3b78c4498f53cca370f835661a60f5c675e8f
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2024-04-06 17:04:26 +0900

    캘린더 구현하긴 했다..

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useMemo, useState } from "react";
 import classes from "./Calendar.module.scss";
 import dayjs from "dayjs";
 import "dayjs/locale/ko";
@@ -14,7 +14,11 @@ const Calendar = () => {
   // 선택한 날짜 월의 첫번쨰 날짜
   const firstDayOfMonth = dayjs(selectedDay).startOf("month").locale("ko");
   // 선택한 날짜 월의 전체 날자를 저장하는 배열
-  const dates = useMemo(() => makeDates(daysInMonth), [daysInMonth]);
+  const dates = useMemo(
+    () => makeDates(daysInMonth, firstDayOfMonth),
+    [daysInMonth, firstDayOfMonth]
+  );
+  const weekHeaders = makeWeekdays();
 
   // 일자 선택했을 때 실행
   function handleNewDateClick(newDate) {
@@ -22,7 +26,7 @@ const Calendar = () => {
   }
 
   // 해당 월의 시작일을 받아서 해당 월의 전체 일자 배열을 반환하는 함수
-  function makeDates(daysInMonth) {
+  function makeDates(daysInMonth, firstDayOfMonth) {
     const dates = [];
 
     for (let i = 0; i < daysInMonth; i++) {
@@ -30,7 +34,36 @@ const Calendar = () => {
       dates.push(date);
     }
 
+    switch (firstDayOfMonth.format("ddd")) {
+      case "일":
+        break;
+      case "토":
+        dates.unshift("");
+      case "금":
+        dates.unshift("");
+      case "목":
+        dates.unshift("");
+      case "수":
+        dates.unshift("");
+      case "화":
+        dates.unshift("");
+      case "월":
+        dates.unshift("");
+        break;
+      default:
+        break;
+    }
+
     return dates;
+  }
+
+  // 요일 배열 얻기 (일월화수목금토)
+  function makeWeekdays() {
+    const weekdays = [];
+    for (let i = 0; i < 7; i++) {
+      weekdays.push(dayjs().day(i).format("ddd"));
+    }
+    return weekdays;
   }
 
   // selectedDay를 이전달로 변경
@@ -48,22 +81,33 @@ const Calendar = () => {
   return (
     <div className={classes.calendar_wrapper}>
       <header className={classes.calendar_header_wrapper}>
-        <div>선택한 날짜 : {selectedDay.format("YYYY-MM-DD ddd")}</div>
-        <button onClick={handleGetPrevMonth}>이전 달로 가기</button>
-        <button onClick={handleGetNextMonth}>다음 달로 가기</button>
+        <button onClick={handleGetPrevMonth}>{`<`}</button>
+        <div>{selectedDay.format("YYYY-MM-DD ddd")}</div>
+        <button onClick={handleGetNextMonth}>{`>`}</button>
+      </header>
+      <header className={classes.calendar_day_of_week_label_wrapper}>
+        {weekHeaders.map((ddd, index) => (
+          <div key={index}>{ddd}</div>
+        ))}
       </header>
       <main className={classes.calendar_main_wrapper}>
         {dates.map((date, index) => {
+          if (date === "") return <div></div>;
           return (
             <div
               key={index}
-              className={`${classes.day_item} ${
-                date.format("YYYYMMDD") === selectedDay.format("YYYYMMDD") &&
-                classes.selected
-              } ${
+              className={`${classes.day_item}  
+              ${
                 date.format("YYYYMMDD") === today.format("YYYYMMDD") &&
                 classes.today
-              } ${date.format("ddd") === "일" && classes.sunday}`}
+              } 
+              ${date.format("ddd") === "일" && classes.sunday} ${
+                date.format("ddd") === "토" && classes.saturday
+              } 
+              ${
+                date.format("YYYYMMDD") === selectedDay.format("YYYYMMDD") &&
+                classes.selected
+              }`}
               onClick={() => {
                 handleNewDateClick(date);
               }}
